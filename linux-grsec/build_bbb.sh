831e8fd512a27788b67432466b54f318e2506170
BrianSidebotham <brian.sidebotham@gmail.com>
2016-04-29 16:03:50 +0100

    * Add Alpine Linux patch set for 4.1.20 (3.3.3)

--- linux-grsec/build_bbb.sh
+++ linux-grsec/build_bbb.sh
@@ -4,7 +4,23 @@ SCRIPTDIR=$(dirname $(readlink -f $0))
 . ${SCRIPTDIR}/settings.sh
 
 echo "Starting Build..."
-
+patches="fix-spi-nor-namespace-clash.patch
+    imx6q-no-unclocked-sleep.patch
+    keys-fixes.patch
+    ovl-fix-getcwd-failure-after-unsuccessful-rmdir.patch
+    ovl-fix-permission-checking-for-setattr.patch
+    staging-dgnc-fix-info-leak-in-ioctl.patch
+    via-velocity-length-check.patch
+    xsa155-linux-xsa155-0001-xen-Add-RING_COPY_REQUEST.patch
+    xsa155-linux-xsa155-0002-xen-netback-don-t-use-last-request-to-determine-mini.patch
+    xsa155-linux-xsa155-0003-xen-netback-use-RING_COPY_REQUEST-throughout.patch
+    xsa155-linux-xsa155-0004-xen-blkback-only-read-request-operation-from-shared-.patch
+    xsa155-linux-xsa155-0006-xen-scsiback-safely-copy-requests.patch
+    xsa155-linux43-0005-xen-blkback-read-from-indirect-descriptors-only-once.patch
+    xsa157-0001-xen-pciback-Return-error-on-XEN_PCI_OP_enable_msi-wh.patch
+    xsa157-0003-xen-pciback-Do-not-install-an-IRQ-handler-for-MSI-in.patch
+    xsa157-0004-xen-pciback-For-XEN_PCI_OP_disable_msi-x-only-disabl.patch
+    "
 if [ ! -d ${KERN_BUILD_DIR} ]; then
     mkdir -p ${KERN_BUILD_DIR}
 fi
@@ -32,7 +48,10 @@ if [ ! -d ${KERN_SOURCE_DIR} ]; then
         tar xJf ${KERN_BUILD_DIR}/${KERNEL_SOURCE_ARCHIVE}
         cd ${KERN_SOURCE_DIR}
         patch -p1 < ${KERN_BUILD_DIR}/${GRSEC_PATCH}
-	patch -p1 < ${SCRIPTDIR}/fix-spi-nor-namespace-clash.patch
+        echo "Patching alpine patches..."
+        for p in ${patches}; do
+            patch -p1 < ${SCRIPTDIR}/patches/${p}
+        done
         touch ${KERN_SOURCE_DIR}/patched
     fi
 fi
@@ -40,7 +59,7 @@ fi
 echo "Building the kernel..."
 cd ${KERN_SOURCE_DIR}
 #make ARCH=arm CROSS_COMPILE=${CC} distclean DISABLE_PAX_PLUGINS=y LOCALVERSION=-grsec
-#cp -v ${BASE}/config-grsec.armhf ${KERN_SOURCE_DIR}/.config LOCALVERSION=-grsec
+cp -v ${BASE}/config-grsec.armhf ${KERN_SOURCE_DIR}/.config LOCALVERSION=-grsec
 #cp -v ${BASE}/config-grsec.bbb ${KERN_SOURCE_DIR}/.config
 #make ARCH=arm CROSS_COMPILE=${CC} omap2plus_defconfig DISABLE_PAX_PLUGINS=y LOCALVERSION=-grsec
 #make ARCH=arm CROSS_COMPILE=${CC} silentoldconfig DISABLE_PAX_PLUGINS=y
